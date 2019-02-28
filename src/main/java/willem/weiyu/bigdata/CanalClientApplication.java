a7573757b7996b93b07d19580be924030229c0ff
weiyu <weiyu005@ke.com>
2019-02-28 15:04:05 +0800

    使用configuration相关属性注解

--- src/main/java/willem/weiyu/bigdata/CanalClientApplication.java
+++ src/main/java/willem/weiyu/bigdata/CanalClientApplication.java
@@ -2,6 +2,8 @@ package willem.weiyu.bigdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import willem.weiyu.bigdata.config.CanalClientConfig;
 
 /**
  * @Author weiyu
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2019/2/19 16:55
  */
 @SpringBootApplication(scanBasePackages = "willem.weiyu.bigdata")
+@EnableConfigurationProperties(CanalClientConfig.class)
 public class CanalClientApplication {
 
     public static void main(String[] args) {
--- src/main/java/willem/weiyu/bigdata/config/CanalClientConfig.java
+++ src/main/java/willem/weiyu/bigdata/config/CanalClientConfig.java
@@ -1,7 +1,7 @@
 package willem.weiyu.bigdata.config;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -10,26 +10,20 @@ import org.springframework.context.annotation.Configuration;
  * @Date 2019/2/19 16:59
  */
 @Configuration
+@ConfigurationProperties(prefix = "canal.client")
 @Data
 public class CanalClientConfig {
-    @Value("${canal.client.isCluster}")
     private boolean isCluster;
 
-    @Value("${canal.client.zkServers}")
     private String zkServers;
 
-    @Value("${canal.client.url}")
     private String url;
 
-    @Value("${canal.client.port}")
     private Integer port;
 
-     @Value("${canal.client.destination}")
     private String destination;
 
-    @Value("${canal.client.username}")
     private String username;
 
-    @Value("${canal.client.password}")
     private String password;
 }
